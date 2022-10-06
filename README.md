953adaa746bf7c5b90bd5694e4f3c18ce0251a29
JhousefSilva1 <50475377+JhousefSilva1@users.noreply.github.com>
2022-10-06 12:18:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prueba-VUE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-uuwy7g)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+   
+    <p>
+     probando el funcionamiento de la herramienta de Vue<br />
+      check out the
+
+      <img class="img1"
+        src="https://red.computerworld.es/archivos/202101/glenn-carstens-peters-npxxwgq33zq-unsplash-1-.jpg"
+        alt=""
+      />
+      <p>
+      Esta es una imagen de prueba
+      </p>
+
+
+
+
+     
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.img1 {
+  width: 250px;
+  height: 250px;
+}
+</style>
